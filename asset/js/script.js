1d93650c4bd417c88613bdd220eb56b57d120387
Tina Nguyen <nguyentinaca@yahoo.com>
2020-12-20 11:56:30 -0800

    start timer

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,40 +1,52 @@
-var questions = [
-    {q: "Commonly used data types DO NOT include:",
-     c: ["strings", "booleans", "alerts", "numbers"],
-     a: "alerts"
+var questionList = [
+    {question: "Commonly used data types DO NOT include:",
+     choices: ["strings", "booleans", "alerts", "numbers"],
+     answer: "alerts"
     },
-    {q: "The condition in an if/else statement is enclosed within ____.",
-     c: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-     a: "parenthesis"
+    {question: "The condition in an if/else statement is enclosed within ____.",
+     choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+     answer: "parenthesis"
     },
-    {q: "Arrays in Javascript can be used to store _____.",
-     c: ["numbers & strings", "other arrays", "booleans", "all of the above"],
-     a: "all of the above"
+    {question: "Arrays in Javascript can be used to store _____.",
+     choices: ["numbers & strings", "other arrays", "booleans", "all of the above"],
+     answer: "all of the above"
     },
-    {q: "String values must be enclose within ____ when being assigned to variable ..",
-     c: ["commas", "curly brackets", "quotes", "parenthesis"],
-     a: "quotes"
+    {question: "String values must be enclose within ____ when being assigned to variable ..",
+     choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+     answer: "quotes"
     },
-    {q: "A very useful tool for used during development and debugging for printing content to the debugger is:",
-     c: ["Javascript", "terminal/bash", "for loops", "console log"],
-     a: "console log"
-    },
+    {question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+     choices: ["Javascript", "terminal/bash", "for loops", "console log"],
+     answer: "console log"
+    }
 ];
 
 //keep track of score
 var score = 0; 
-var questionList = 0
+var currentQuestion = -1
+var timeLeft = 75;
+var timer;
 
-// function to start the quiz
-function startquiz() {
+// function to start the time
+function startTimer() {
 
-    //start timer 
-    timer
-}
+   
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+    
 
+    timer = setInterval(function() {
+        timeLeft--;
+        document.getElementById("timeLeft").innerHTML = timeLeft;
 
+        //end game when timer is below 0
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            endGame(); 
+        }
+    }, 1000);
 
+    startquiz();
+    }
 
-// start quiz when user clicks button
-var startquizBtn = document.querySelector("#start-quiz");
-startquizBtn.addEventListener("click", startquiz);
+    
+
