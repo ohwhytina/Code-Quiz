6b7aa7f9edb268b481ac40cf07f664b79fbb8404
Tina Nguyen <nguyentinaca@yahoo.com>
2020-12-20 08:05:09 -0800

    page templete and quiz questions

---
+++ asset/js/script.js
@@ -0,0 +1,40 @@
+var questions = [
+    {q: "Commonly used data types DO NOT include:",
+     c: ["strings", "booleans", "alerts", "numbers"],
+     a: "alerts"
+    },
+    {q: "The condition in an if/else statement is enclosed within ____.",
+     c: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+     a: "parenthesis"
+    },
+    {q: "Arrays in Javascript can be used to store _____.",
+     c: ["numbers & strings", "other arrays", "booleans", "all of the above"],
+     a: "all of the above"
+    },
+    {q: "String values must be enclose within ____ when being assigned to variable ..",
+     c: ["commas", "curly brackets", "quotes", "parenthesis"],
+     a: "quotes"
+    },
+    {q: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+     c: ["Javascript", "terminal/bash", "for loops", "console log"],
+     a: "console log"
+    },
+];
+
+//keep track of score
+var score = 0; 
+var questionList = 0
+
+// function to start the quiz
+function startquiz() {
+
+    //start timer 
+    tim
+}
+
+
+
+
+// start quiz when user clicks button
+var startquizBtn = document.querySelector("#start-quiz");
+startquizBtn.addEventListener("click", startquiz);
