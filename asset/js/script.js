a6ada3be838321505a539eb7efea86f78f4a4ff0
Tina Nguyen <nguyentinaca@yahoo.com>
2020-12-20 17:39:09 -0800

    quiz function display and working

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,21 +1,21 @@
-var questionList = [
-    {question: "Commonly used data types DO NOT include:",
+var questions = [
+    {title: "Commonly used data types DO NOT include:",
      choices: ["strings", "booleans", "alerts", "numbers"],
      answer: "alerts"
     },
-    {question: "The condition in an if/else statement is enclosed within ____.",
+    {title: "The condition in an if/else statement is enclosed within ____.",
      choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
      answer: "parenthesis"
     },
-    {question: "Arrays in Javascript can be used to store _____.",
+    {title: "Arrays in Javascript can be used to store _____.",
      choices: ["numbers & strings", "other arrays", "booleans", "all of the above"],
      answer: "all of the above"
     },
-    {question: "String values must be enclose within ____ when being assigned to variable ..",
+    {title: "String values must be enclose within ____ when being assigned to variable.",
      choices: ["commas", "curly brackets", "quotes", "parenthesis"],
      answer: "quotes"
     },
-    {question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+    {title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
      choices: ["Javascript", "terminal/bash", "for loops", "console log"],
      answer: "console log"
     }
@@ -29,11 +29,8 @@ var timer;
 
 // function to start the time
 function startTimer() {
-
-   
     document.getElementById("timeLeft").innerHTML = timeLeft;
     
-
     timer = setInterval(function() {
         timeLeft--;
         document.getElementById("timeLeft").innerHTML = timeLeft;
@@ -45,8 +42,60 @@ function startTimer() {
         }
     }, 1000);
 
-    startquiz();
+    startQuiz();
+};
+
+//stop the timer to end the game 
+function endGame() {
+    clearInterval(timer);
+
+    var endingContent = 
+    "<h2>" + "Game over!" + "</h2>" +
+    "<h3>" + "You got a " + score +  "/5" + " questions correct!" + "</h3>" +
+    `<input type="text" id="name" placeholder="Enter name">
+    <button onclick="setScore()">Set score!</button>`;
+
+    document.getElementById("quizBody").innerHTML = endingContent;
+}
+
+
+
+function startQuiz() {
+    currentQuestion++;
+
+    if (currentQuestion > questions.length -1) {
+        endGame();
+        return
+    }
+    //show question
+    var questionContent = "<h2>" + questions[currentQuestion].title + "</h2>"; 
+    
+    // loop questions
+    for (var i = 0; i < questions[currentQuestion].choices.length; i++) {
+        // create choices buttons, when click refers to answer array
+        var buttonChoice = "<button onclick=[ANSWER]>[CHOICE]</button>"; 
+        buttonChoice = buttonChoice.replace("[CHOICE]", questions[currentQuestion].choices[i]);
+        
+        if (questions[currentQuestion].choices[i] === questions[currentQuestion].answer) {
+            buttonChoice = buttonChoice.replace("[ANSWER]", "correct()");
+        } else {
+            buttonChoice = buttonChoice.replace("[ANSWER]", "incorrect()");
+        }
+        questionContent += buttonChoice
     }
 
-    
 
+    document.getElementById("quizBody").innerHTML = questionContent
+};
+
+//deduct 10 seconds from the timer if user chooses an incorrect answer
+function incorrect() {
+    timeLeft -= 10; 
+    startQuiz();
+}
+
+//increases score by 1 points if the user chooses the correct answer
+function correct() {
+    score += 1;
+    startQuiz();
+}
